197d4980495292b275ff12ef536b6a17f40bc048
ranim.q <raneemkador@gmail.com>
2018-04-09 23:27:05 +0300

    add claim

--- test1/src/com/claim/view/beans/ClaimBean.java
+++ test1/src/com/claim/view/beans/ClaimBean.java
@@ -8,7 +8,11 @@ import javax.faces.bean.SessionScoped;
 
 import com.claim.generic.view.utils.BeanUtility;
 import com.claim.model.entity.Claim;
+import com.claim.model.entity.ClaimType;
+import com.claim.model.entity.SysUser;
 import com.claim.model.service.ClaimService;
+import com.claim.model.service.ClaimTypeService;
+import com.claim.model.service.SysUserService;
 
 @ManagedBean
 @SessionScoped
@@ -21,6 +25,9 @@ public class ClaimBean implements Serializable {
 	
 	private Claim claim;
 	private ClaimService claimService;
+	private SysUserService sysUserService;
+	private ClaimTypeService claimTypeService;
+	private String claimTypeString;
 	
 	@PostConstruct
 	public void init() {
@@ -28,6 +35,8 @@ public class ClaimBean implements Serializable {
 	public ClaimBean(){
 		claim = new Claim();
 		claimService = (ClaimService) BeanUtility.getBean("claimService");
+		sysUserService = (SysUserService) BeanUtility.getBean("sysUserService");
+		claimTypeService = (ClaimTypeService) BeanUtility.getBean("claimTypeService");
 	}
 
 	
@@ -58,7 +67,21 @@ public class ClaimBean implements Serializable {
 		this.claimService = claimService;
 	}
 
+	public String getClaimType() {
+		return claimTypeString;
+	}
+	public void setClaimType(String claimTypeString) {
+		this.claimTypeString = claimTypeString;
+	}
 	public void addClaim(){
+		ClaimType claimType = new ClaimType();
+		SysUser sysUser = new SysUser();
+		Integer id = 1;
+		Short claimTypeid = Short.parseShort(claimTypeString);;
+		sysUser = sysUserService.getById(id);
+		claimType = claimTypeService.getById(claimTypeid);
+		claim.setClaimType(claimType);
+		claim.setSysUser(sysUser);
 		claimService.insert(claim);
 	}
 	
